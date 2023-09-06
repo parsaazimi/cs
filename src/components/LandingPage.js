15dde11e6361c8fa238362fc3d6f2660311ba590
ParsaAzimi23 <pazimi730@gmail.com>
2023-09-06 21:30:23 +0330

    folder structuring done

---
+++ src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+    return (
+        <div className='justify-between'>
+            <div>
+                <Link className="">ثبت نام</Link>
+                <Link>ورود</Link>
+            </div>
+            <div>
+                hey
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
