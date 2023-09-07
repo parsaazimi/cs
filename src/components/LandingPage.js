9c25447cb8d3269f7ae24af7ca9c829909abfaea
ParsaAzimi23 <pazimi730@gmail.com>
2023-09-08 01:27:31 +0330

    Navigation bar created,needs routing!

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import NavBar from './NavBar';
 
 const LandingPage = () => {
     return (
-        <div className='justify-between'>
-            <div>
-                <Link className="">ثبت نام</Link>
-                <Link>ورود</Link>
-            </div>
-            <div>
-                hey
-            </div>
+        <div>
+            <NavBar />
         </div>
     );
 };
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import purpleLogo from '../assets/logo-purple.png'
+const NavBar = () => {
+    return (
+        <div className='px-[117px] flex justify-between h-[72px] items-center shadow-[0_4px_6px_-4px_rgba(0,0,0,0.25)]'>
+            <div className='flex items-center'>
+                <Link><img className='w-[42px] ml-[40px]' src={purpleLogo} alt='purple-logo' /></Link>
+                <Link className='ml-7 font-bold'>خانه </Link>
+                <Link className='ml-7 font-bold'>دوره ها </Link>
+                <Link className='ml-7 font-bold'>اخبار </Link>
+                <Link className='ml-7 font-bold'>درباره ما </Link>
+                <Link className='font-bold'>تماس با ما</Link>
+            </div>
+            <div>
+                <Link>circle</Link>
+            </div>
+            <div>
+                <Routes>
+                    
+                </Routes>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
